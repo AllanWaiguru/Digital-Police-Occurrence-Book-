aeb5df50ab0009b11e1a207b930c9ed834978209
Joseph Kariuki <joehene@gmail.com>
2024-03-20 08:12:29 +0300

    Created views and templates

--- obook_app/urls.py
+++ obook_app/urls.py
@@ -1,6 +1,18 @@
 from django.urls import path
-from .views import LandingPageView
+from .views import (
+    LandingPageView,
+    AboutPageView,
+    FaqPageView,
+    ContactPageView,
+    BookDetailPageView,
+    BookListPageView,
+)
 
 urlpatterns = [
-    path('', LandingPageView.as_view(), name='index'),
+    path("", LandingPageView.as_view(), name="index"),
+    path("about/", AboutPageView.as_view(), name="about"),
+    path("contact/", ContactPageView.as_view(), name="contact"),
+    path("faq/", FaqPageView.as_view(), name="faq"),
+    path("occurences/", BookListPageView.as_view(), name="occurences"),
+    path("occurence/<int:pk>/", BookDetailPageView.as_view(), name="occurence_detail"),
 ]
--- occurrence_book/settings.py
+++ occurrence_book/settings.py
@@ -117,12 +117,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 AUTH_USER_MODEL = "users.CustomUser"
-
-STATIC_ROOT = 'staticfiles/'
